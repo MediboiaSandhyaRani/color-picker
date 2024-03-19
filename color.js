cd29be30f08896b45c443cda8330e855b896353c
Mediboina Sandhya Rani <163757209+MediboiaSandhyaRani@users.noreply.github.com>
2024-03-19 10:15:00 +0530

    commit

---
+++ color.js
@@ -0,0 +1,22 @@
+let colorPickerEle = document.getElementById("bigContainer");
+let textColorEle = document.getElementById("color");
+
+function btn1() {
+    colorPickerEle.style.backgroundColor = "#e0e0e0";
+    textColorEle.textContent = "#e0e0e0";
+}
+
+function btn2() {
+    colorPickerEle.style.backgroundColor = "#6fcf97";
+    textColorEle.textContent = "#6fcf97";
+}
+
+function btn3() {
+    colorPickerEle.style.backgroundColor = "#56ccf2";
+    textColorEle.textContent = "#56ccf2";
+}
+
+function btn4() {
+    colorPickerEle.style.backgroundColor = "#bb6bd9";
+    textColorEle.textContent = "#bb6bd9";
+}
